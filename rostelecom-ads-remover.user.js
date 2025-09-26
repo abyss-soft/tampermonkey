da019f29cb7050284b1533b815682910b81aa88f
Alexander Panov <abyss-soft@yandex.com>
2025-09-26 16:09:19 +0300

    rostelecom

---
+++ rostelecom-ads-remover.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Rostelecom AD remover
+// @namespace    Rostelecom-AD-remover
+// @version      0.1
+// @description  Rostelecom AD remover
+// @author       github.com/abyss-soft
+// @match        http://*/*
+// @grant        none
+// @updateURL    https://github.com/abyss-soft/tampermonkey/blob/main/rostelecom-ads-remover.user.js
+// @downloadURL  https://github.com/abyss-soft/tampermonkey/blob/main/rostelecom-ads-remover.user.js
+// ==/UserScript==
+(function() {
+    'use strict';
+    window.onload = function() {
+        setTimeout(()=>{
+            //скрипт для удаления рекламы от Ростелеком, которые она вставляет на сайты с HTTP
+
+            const arrayID = [
+                '[id^="large-r-"]',
+                '[id^="medium-r-"]',
+                '[id^="txtblock-"]'
+            ];
+            arrayID.forEach(item =>{
+                const element = document.querySelector(item);
+                if(element) {
+                    element.style.display="none";
+                    console.log('Remove: ', element)}
+            })
+        },1000, this);
+    };
+})();
