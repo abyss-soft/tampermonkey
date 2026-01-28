75d29c746a348314784c14892b6cdf1d05b05979
Alexander Panov <abyss-soft@yandex.com>
2026-01-28 15:58:46 +0300

    fix rostelecom-ads

--- rostelecom-injected-ads-remover.user.js
+++ rostelecom-injected-ads-remover.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
-// @name         Rostelecom AD remover
+// @name         Rostelecom Injected Ads Remover
+// @name:en      Rostelecom Injected Ads Remover
 // @namespace    Rostelecom-AD-remover
-// @version      0.1
-// @description  Rostelecom AD remover
+// @version      0.2
+// @description  Removes ads injected by Rostelecom on HTTP websites
+// @description:ru Удаляет рекламу, внедряемую Ростелекомом на HTTP-сайтах
 // @author       github.com/abyss-soft
 // @match        http://*/*
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/abyss-soft/tampermonkey/main/rostelecom-ads-remover.user.js
-// @downloadURL  https://raw.githubusercontent.com/abyss-soft/tampermonkey/main/rostelecom-ads-remover.user.js
+// @updateURL    https://raw.githubusercontent.com/abyss-soft/tampermonkey/main/rostelecom-injected-ads-remover.user.js
+// @downloadURL  https://raw.githubusercontent.com/abyss-soft/tampermonkey/main/rostelecom-injected-ads-remover.user.js
+// @license MIT
 // ==/UserScript==
 (function() {
     'use strict';
