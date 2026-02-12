69afc219e774e4153202566be71042c7767ba903
Alexander Panov <abyss-soft@yandex.com>
2026-02-12 12:01:46 +0300

    hide top banner

--- yandex-Mail-Remove-Ads-and-Banners.user.js
+++ yandex-Mail-Remove-Ads-and-Banners.user.js
@@ -3,9 +3,9 @@
 // @name:ru      Яндекс Почта - Удаление рекламы
 // @name:en      Yandex Mail - Remove Ads & Iframes
 // @namespace    https://github.com/abyss-soft/yandex-Mail-Remove-Ads-and-Banners
-// @version      0.9
+// @version      1.0
 // @description  Удаляет рекламу и баннеры с Yandex Mail (поддерживает SPA, легковесный)
-// @description:en Removes ads, banners and iframe ads from Yandex Mail (SPA-friendly, lightweight) 
+// @description:en Removes ads, banners and iframe ads from Yandex Mail (SPA-friendly, lightweight)
 // @author       github.com/abyss-soft
 // @match        https://mail.yandex.ru/*
 // @match        http://mail.yandex.ru/*
@@ -17,24 +17,18 @@
 // @license MIT
 // ==/UserScript==
 
+
 (function () {
   'use strict';
 
-  GM_addStyle(`
-    /* Верхние баннеры */
-    div:has(> [data-testid="toolbar-layout_container"])
-    > :not([data-testid="toolbar-layout_container"]) {
-      display: none !important;
-    }
-
-    /* Правая колонка */
+  const css = `
+   /* Правая колонка */
     div[data-testid^="page-layout_right-column_"] {
       display: none !important;
     }
 
     /* Роскомнадзор */
-    .Warning_type_roskomnadzor,
-    .Modal {
+    .Warning_type_roskomnadzor {
       display: none !important;
     }
 
@@ -45,15 +39,21 @@
     iframe[src*="banner"] {
       display: none !important;
     }
-  `);
+    `;
 
 
   let scheduled = false;
 
+  function injectStyle() {
+    if (document.getElementById('ymail-remove-ads-style')) return;
+
+    const style = document.createElement('style');
+    style.id = 'ymail-remove-ads-style';
+    style.textContent = css;
+    (document.head || document.documentElement).appendChild(style);
+  }
+
   function removeDynamicAds() {
-    scheduled = false;
-
-
     const btn = [...document.querySelectorAll('a')]
       .find(a => a.textContent.trim() === 'Отключить рекламу');
 
@@ -64,17 +64,32 @@
         block.dataset.hiddenByScript = 'true';
       }
     }
+
+    /* Верхние баннеры */
+    document.querySelectorAll('[id*="js-messages-direct"]').forEach(el => {
+      const parent = el.parentElement;
+      if (parent && !parent.dataset.hiddenByScript) {
+        parent.style.display = 'none';
+        parent.dataset.hiddenByScript = 'true';
+      }
+    });
+
   }
 
   // debounce
   function scheduleCleanup() {
     if (!scheduled) {
       scheduled = true;
-      requestAnimationFrame(removeDynamicAds);
+      requestAnimationFrame(() => {
+        scheduled = false;
+        injectStyle();
+        removeDynamicAds();
+      });
     }
   }
 
   // Убираем сразу
+  injectStyle();
   removeDynamicAds();
 
   // наблюдение за SPA
