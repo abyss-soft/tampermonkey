a1e7ef9e07f0fb9a92814ad381fcc8c6c7a77ce4
Alexander Panov <abyss-soft@yandex.com>
2026-09-11 13:28:18 +0300

    fix start

--- rostelecom-injected-ads-remover.user.js
+++ rostelecom-injected-ads-remover.user.js
@@ -1,34 +1,44 @@
 // ==UserScript==
-// @name         Rostelecom Injected Ads Remover
-// @name:en      Rostelecom Injected Ads Remover
+// @name         Rostelecom AD remover
 // @namespace    Rostelecom-AD-remover
 // @version      0.2
-// @description  Removes ads injected by Rostelecom on HTTP websites
-// @description:ru Удаляет рекламу, внедряемую Ростелекомом на HTTP-сайтах
+// @description  Rostelecom AD remover
 // @author       github.com/abyss-soft
 // @match        http://*/*
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/abyss-soft/tampermonkey/main/rostelecom-injected-ads-remover.user.js
-// @downloadURL  https://raw.githubusercontent.com/abyss-soft/tampermonkey/main/rostelecom-injected-ads-remover.user.js
-// @license MIT
+// @updateURL    https://raw.githubusercontent.com/abyss-soft/tampermonkey/main/rostelecom-ads-remover.user.js
+// @downloadURL  https://raw.githubusercontent.com/abyss-soft/tampermonkey/main/rostelecom-ads-remover.user.js
 // ==/UserScript==
-(function() {
+(function () {
     'use strict';
-    window.onload = function() {
-        setTimeout(()=>{
-            //скрипт для удаления рекламы от Ростелеком, которые она вставляет на сайты с HTTP
 
-            const arrayID = [
-                '[id^="large-r-"]',
-                '[id^="medium-r-"]',
-                '[id^="txtblock-"]'
-            ];
-            arrayID.forEach(item =>{
-                const element = document.querySelector(item);
-                if(element) {
-                    element.style.display="none";
-                    console.log('Remove: ', element)}
-            })
-        },1000, this);
-    };
+
+    function removeRostelecom() {
+        //скрипт для удаления рекламы от Ростелеком, которые она вставляет на сайты с HTTP
+
+        const arrayID = [
+            '[id^="large-r-"]',
+            '[id^="medium-r-"]',
+            '[id^="txtblock-"]'
+        ];
+        arrayID.forEach(item => {
+            const element = document.querySelector(item);
+            if (element) {
+                element.style.display = "none";
+                console.log('Remove: ', element)
+            }
+        })
+    }
+
+    // Слушатель изменений DOM (с debounce для предотвращения лишних срабатываний)
+    let timeoutId = null;
+    const observer = new MutationObserver(() => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(removeRostelecom, 150);
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // Первичный запуск
+    removeRostelecom();
 })();
